8caf5ad8dd307027a19879e91992a04a5d6c1cd5
lydial12 <lydia.rose.lit@gmail.com>
2026-01-17 16:32:45 -0600

    Created spindexer subsystem
    Co-authored-by: BryantPredmore <BryantPredmore@users.noreply.github.com>
    Co-authored-by: Nathan2351 <Nathan2351@users.noreply.github.com>

--- robotbase/src/main/java/frc/robot/controls/CoDriverControls.java
+++ robotbase/src/main/java/frc/robot/controls/CoDriverControls.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import frc.robot.Constants.CONTROLLER;
 import frc.robot.controls.util.RumbleInterface;
+import edu.wpi.first.units.measure.Dimensionless;
 
 public class CoDriverControls implements RumbleInterface {
 
@@ -15,12 +16,36 @@ public class CoDriverControls implements RumbleInterface {
 
   public CoDriverControls() {
     m_controller = new CommandXboxController(
-      CONTROLLER.CODRIVER_CONTROLLER_PORT
-    );
+        CONTROLLER.CODRIVER_CONTROLLER_PORT);
     mapControls();
   }
 
-  public void mapControls() {}
+  public void mapControls() {
+  }
+
+  public Dimensionless getRightX() {
+    return Value.of(modifyAxis(-m_controller.getRightX()));
+  }
+
+  private double modifyAxis(double value) {
+    value = deadband(value, CONTROLLER.DRIVER_CONTROLLER_DEADBAND);
+    value = Math.copySign(
+        Math.pow(value, Constants.CONTROLLER.JOYSTICK_RAMP_EXPONENT),
+        value);
+    return value;
+  }
+
+  private double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
 
   public Dimensionless getRightX() {
     return Value.of(modifyAxis(-m_controller.getRightX()));
