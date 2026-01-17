4ee4a091bb6121b8e3f1bdfe35eda76c4efa9187
lydial12 <lydia.rose.lit@gmail.com>
2026-01-17 10:22:13 -0600

    Created tuning

---
+++ Tuning/src/main/java/frc/robot/util/Utility.java
@@ -0,0 +1,19 @@
+package frc.robot.util;
+
+public class Utility {
+
+  public static boolean isWithinTolerance(
+    double currentValue,
+    double targetValue,
+    double tolerance
+  ) {
+    return Math.abs(currentValue - targetValue) <= tolerance;
+  }
+
+  public static double deadband(double input, double deadband) {
+    if (Math.abs(input) < deadband) {
+      return 0;
+    }
+    return input;
+  }
+}
