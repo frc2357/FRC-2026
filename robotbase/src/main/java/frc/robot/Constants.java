2c82b8d862ebcc14c3ba0d11bbe552aea5b76d8e
lydial12 <lydia.rose.lit@gmail.com>
2026-01-17 17:49:24 -0600

    Remove Spindexer from tuning branch

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -26,15 +26,14 @@ public class Constants {
 
   public static final class SWERVE {
 
-    public static final AngularVelocity MAX_ANGULAR_RATE = RotationsPerSecond.of(1);
-    public static final LinearVelocity MAX_SPEED = TunerConstants.kSpeedAt12Volts;
+    public static final AngularVelocity MAX_ANGULAR_RATE =
+      RotationsPerSecond.of(1);
+    public static final LinearVelocity MAX_SPEED =
+      TunerConstants.kSpeedAt12Volts;
 
     public static final Dimensionless AXIS_MAX_ANGULAR_RATE = Units.Percent.of(
-        50);
+      50
+    );
     public static final Dimensionless AXIS_MAX_SPEED = Units.Percent.of(50);
-  }
-
-  public static final class CAN_ID {
-    public static final int SPINDEXER_ID = 1; // TODO: Figure out CAN_ID
   }
 }
