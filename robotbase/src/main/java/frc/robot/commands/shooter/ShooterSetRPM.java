3fd90d38c87c0e00a986a4e257a6a76ecc887ffd
Lydia <lydia.rose.lit@gmail.com>
2026-02-01 15:54:19 -0600

    Shooter subsystem (#55)

---
+++ robotbase/src/main/java/frc/robot/commands/shooter/ShooterSetRPM.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+public class ShooterSetRPM extends Command {
+
+  private AngularVelocity m_targetVelocity;
+
+  public ShooterSetRPM(AngularVelocity targetVelocity) {
+    addRequirements(Robot.shooter);
+    m_targetVelocity = targetVelocity;
+  }
+
+  @Override
+  public void initialize() {
+    Robot.shooter.setTargetVelocity(m_targetVelocity);
+  }
+
+  @Override
+  public void execute() {
+    Robot.shooter.updateMotorPIDs();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    Robot.shooter.stop();
+  }
+}
