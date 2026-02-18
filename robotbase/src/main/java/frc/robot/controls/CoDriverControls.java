b1a559d515fb63adbeb424fbd7dbffceb0100402
BryantPredmore <predmorebryant1@gmail.com>
2026-02-17 19:33:18 -0600

    Bryant Added Controller Bindings (#56)

--- robotbase/src/main/java/frc/robot/controls/CoDriverControls.java
+++ robotbase/src/main/java/frc/robot/controls/CoDriverControls.java
@@ -4,9 +4,14 @@ import static edu.wpi.first.units.Units.Value;
 
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
 import frc.robot.Constants.CONTROLLER;
+import frc.robot.Robot;
+import frc.robot.commands.StopAllMotors;
+import frc.robot.commands.intake.IntakeAxis;
 import frc.robot.controls.util.RumbleInterface;
 
 public class CoDriverControls implements RumbleInterface {
@@ -20,19 +25,112 @@ public class CoDriverControls implements RumbleInterface {
     mapControls();
   }
 
-  public void mapControls() {}
+  public void mapControls() {
+    Trigger noDpad = m_controller
+      .povUp()
+      .negate()
+      .and(m_controller.povRight().negate())
+      .and(m_controller.povLeft().negate())
+      .and(m_controller.povDown().negate());
 
-  public Dimensionless getRightX() {
-    return Value.of(modifyAxis(-m_controller.getRightX()));
+    Trigger onlyLeft = m_controller
+      .povUp()
+      .negate()
+      .and(m_controller.povRight().negate())
+      .and(m_controller.povLeft())
+      .and(m_controller.povDown().negate());
+
+    Trigger onlyRight = m_controller
+      .povUp()
+      .negate()
+      .and(m_controller.povRight())
+      .and(m_controller.povLeft().negate())
+      .and(m_controller.povDown().negate());
+
+    Trigger onlyUp = m_controller
+      .povUp()
+      .and(m_controller.povRight().negate())
+      .and(m_controller.povLeft().negate())
+      .and(m_controller.povDown().negate());
+
+    Trigger onlyDown = m_controller
+      .povUp()
+      .negate()
+      .and(m_controller.povRight().negate())
+      .and(m_controller.povLeft().negate())
+      .and(m_controller.povDown());
+    Trigger noLetterButtons = m_controller
+      .a()
+      .negate()
+      .and(m_controller.b().negate())
+      .and(m_controller.x().negate())
+      .and(m_controller.y().negate());
+
+    m_controller.start().onTrue(new StopAllMotors());
+
+    // change WaitCommad to ShooterHoodAxis once it is finshed
+    onlyUp.whileTrue(new WaitCommand(0));
+
+    onlyUp
+      .and(m_controller.rightTrigger())
+      .whileTrue(
+        Robot.shooter.axisSpeed(() ->
+          Value.of(m_controller.getRightTriggerAxis())
+        )
+      );
+
+    onlyUp
+      .and(m_controller.leftTrigger())
+      .whileTrue(
+        Robot.shooter.axisSpeed(() ->
+          Value.of(-m_controller.getLeftTriggerAxis())
+        )
+      );
+
+    onlyLeft
+      .and(m_controller.leftTrigger())
+      .whileTrue(
+        new IntakeAxis(() -> Value.of(-m_controller.getLeftTriggerAxis()))
+      );
+
+    onlyLeft
+      .and(m_controller.rightTrigger())
+      .whileTrue(
+        new IntakeAxis(() -> Value.of(m_controller.getRightTriggerAxis()))
+      );
   }
 
   private double modifyAxis(double value) {
-    value = deadband(value, CONTROLLER.DRIVER_CONTROLLER_DEADBAND);
+    value = deadband(value, CONTROLLER.CODRIVER_CONTROLLER_DEADBAND);
     value = Math.copySign(
       Math.pow(value, Constants.CONTROLLER.JOYSTICK_RAMP_EXPONENT),
       value
     );
     return value;
+  }
+
+  public Dimensionless getRightX() {
+    return Value.of(modifyAxis(m_controller.getRightX()));
+  }
+
+  public Dimensionless getLeftX() {
+    return Value.of(modifyAxis(m_controller.getLeftX()));
+  }
+
+  public Dimensionless getRightY() {
+    return Value.of(modifyAxis(-m_controller.getRightY()));
+  }
+
+  public Dimensionless getLeftY() {
+    return Value.of(modifyAxis(-m_controller.getLeftY()));
+  }
+
+  public Dimensionless getRightTrigger() {
+    return Value.of(m_controller.getRightTriggerAxis());
+  }
+
+  public Dimensionless getLeftTrigger() {
+    return Value.of(m_controller.getLeftTriggerAxis());
   }
 
   private double deadband(double value, double deadband) {
