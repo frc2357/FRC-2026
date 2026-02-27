d7aa1402c0951bdb6a7bf9295e7ecdb1fa6f43c8
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2026-02-26 19:55:08 -0600

    fixed 4 forgotten unsaved files

--- robotbase/src/main/java/frc/robot/commands/floor/FloorStop.java
+++ robotbase/src/main/java/frc/robot/commands/floor/FloorStop.java
@@ -1,11 +1,11 @@
-package frc.robot.commands.spindexer;
+package frc.robot.commands.floor;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 
-public class SpindexerStop extends Command {
+public class FloorStop extends Command {
 
-  public SpindexerStop() {
+  public FloorStop() {
     addRequirements(Robot.floor);
   }
 
