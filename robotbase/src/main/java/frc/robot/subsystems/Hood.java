0874c9cf5d9ef163a5e230876ab9d27b25a529cb
Nathan2351 <nathan.larsen892@raypec.org>
2026-03-01 14:03:39 -0600

    Remove unused imports

--- robotbase/src/main/java/frc/robot/subsystems/Hood.java
+++ robotbase/src/main/java/frc/robot/subsystems/Hood.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Percent;
 import static edu.wpi.first.units.Units.Value;
 
 import com.revrobotics.spark.SparkAbsoluteEncoder;
@@ -10,7 +9,6 @@ import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Dimensionless;
-import edu.wpi.first.wpilibj.PS4Controller.Axis;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
