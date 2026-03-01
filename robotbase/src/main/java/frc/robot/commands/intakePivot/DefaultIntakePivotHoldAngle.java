855574ca4420300f62ace7c3ea266b31241e3779
Nolan Campbell <nolanc1223@gmail.com>
2026-03-01 13:59:25 -0600

    Fix name

--- robotbase/src/main/java/frc/robot/commands/intakePivot/DefaultIntakePivotHoldAngle.java
+++ robotbase/src/main/java/frc/robot/commands/intakePivot/DefaultIntakePivotHoldAngle.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.intake;
+package frc.robot.commands._intakeivot;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
--- robotbase/src/main/java/frc/robot/commands/intakePivot/DefaultIntakePivotPercentOutput.java
+++ robotbase/src/main/java/frc/robot/commands/intakePivot/DefaultIntakePivotPercentOutput.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.intake;
+package frc.robot.commands._intakeivot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.INTAKE_PIVOT;
