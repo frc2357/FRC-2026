151f00ad5b8c70b6b9e996c2d02b82eb7b06381d
Nathan2351 <nathan.larsen892@raypec.org>
2026-03-05 20:06:57 -0600

    Got all of the basic things in for Tunnel Subsystem

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -268,6 +268,8 @@ public class Constants {
   public static final class CAN_ID {
 
     public static final int FLOOR_MOTOR = 23; //TODO: make sure that the old spindexter motor is the same for the floor (or just change the CAN ID)
+    public static final int TUNNEL_MOTOR = 34;
+    //TODO: add CAN_ID
 
     public static final int LEFT_INTAKE_MOTOR = 24;
     public static final int RIGHT_INTAKE_MOTOR = 25;
@@ -290,8 +292,19 @@ public class Constants {
       .openLoopRampRate(0.25); // TODO: double check these values
 
     public static final Dimensionless AXIS_MAX_SPEED = Percent.of(100);
+    //Just an idea but add a max speed so it doesn't rip itself to shreds
 
     public static final Dimensionless FLOOR_SPEED = Percent.of(50);
+  }
+
+  public static final class TUNNEL {
+
+    public static final SparkBaseConfig MOTOR_CONFIG = new SparkMaxConfig()
+      .idleMode(IdleMode.kCoast)
+      .inverted(false);
+
+    public static final Dimensionless AXIS_MAX_SPEED = Percent.of(100);
+    public static final Dimensionless TUNNEL_SPEED = Percent.of(80);
   }
 
   public static final class INTAKE {
