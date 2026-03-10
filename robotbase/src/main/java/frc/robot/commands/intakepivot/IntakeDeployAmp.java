e2a883c00d333e2d36db3123bc4d2d00b2f8a631
Nolan Campbell <nolanc1223@gmail.com>
2026-03-10 17:52:58 -0500

    Remove unused import

--- robotbase/src/main/java/frc/robot/commands/intakepivot/IntakeDeployAmp.java
+++ robotbase/src/main/java/frc/robot/commands/intakepivot/IntakeDeployAmp.java
@@ -1,6 +1,5 @@
 package frc.robot.commands.intakepivot;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
