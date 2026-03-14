01f56557c8e69c981a482d1e343d4e1a40b091ff
Tyson453 <tyson112305@gmail.com>
2026-03-14 17:10:37 -0500

    cleanup so we're only using one calculator helper

--- robotbase/src/main/java/frc/robot/commands/scoring/VisionScore.java
+++ robotbase/src/main/java/frc/robot/commands/scoring/VisionScore.java
@@ -2,7 +2,6 @@ package frc.robot.commands.scoring;
 
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.commands.drive.DrivePoseTargetingHub;
 import java.util.function.Supplier;
@@ -12,12 +11,10 @@ public class VisionScore extends ParallelCommandGroup {
   public VisionScore(Supplier<Dimensionless> x, Supplier<Dimensionless> y) {
     super(
       new Score(
-        () -> Robot.scoreCalculator.getCalculatedShooterVelocity(),
-        () -> Robot.scoreCalculator.getCalculatedHoodAngle()
+        () -> Robot.shotCalculator.getCalculatedShooterVelocity(),
+        () -> Robot.shotCalculator.getCalculatedHoodAngle()
       ),
-      new DrivePoseTargetingHub(x, y).alongWith(
-        new SetShotTarget(Constants.FieldConstants.Hub.centerPoint)
-      )
+      new DrivePoseTargetingHub(x, y)
     );
   }
 }
