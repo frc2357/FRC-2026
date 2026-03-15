2d1b62161c8d160ea8927f507548e36d1dfbe7b0
lydial12 <lydia.rose.lit@gmail.com>
2026-03-15 16:52:21 -0500

    build and remformat?

--- robotbase/src/main/java/frc/robot/commands/intakepivot/IntakePivotJiggle.java
+++ robotbase/src/main/java/frc/robot/commands/intakepivot/IntakePivotJiggle.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.intakepivot;
+package frc.robot.commands.intakePivot;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
