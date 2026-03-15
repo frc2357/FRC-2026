97355eebecc22f56ea33ac6b18857cc47498d68a
lydial12 <lydia.rose.lit@gmail.com>
2026-03-15 17:21:14 -0500

    fixed errors

--- robotbase/src/main/java/frc/robot/commands/intakePivot/IntakePivotJiggle.java
+++ robotbase/src/main/java/frc/robot/commands/intakePivot/IntakePivotJiggle.java
@@ -5,26 +5,23 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
 import frc.robot.Robot;
 
 public class IntakePivotJiggle extends RepeatCommand {
 
-  public IntakePivotJiggle(Angle topAngle, Angle bottomAngle) {
+  public IntakePivotJiggle() {
     super(
       new SequentialCommandGroup(
-        new ParallelDeadlineGroup(
-          new WaitUntilCommand(() ->
-            Robot.intakePivot.getAngle().isNear(topAngle, .1)
-          ),
-          Robot.intakePivot.setAngle(topAngle)
+        new ParallelRaceGroup(
+          Robot.intakePivot.setSpeed(Constants.INTAKE_PIVOT.JIGGLE_UP_SPEED),
+          new WaitUntilIntakePivotStall(),
+          new WaitCommand(Constants.INTAKE_PIVOT.JIGGLE_UP_TIME)
         ),
         new ParallelRaceGroup(
-          Robot.intakePivot.setAngle(bottomAngle),
-          new WaitUntilCommand(() ->
-            Robot.intakePivot.getAngle().isNear(bottomAngle, .1)
-          ),
-          Robot.intakePivot.setAngle(bottomAngle)
+          Robot.intakePivot.setSpeed(Constants.INTAKE_PIVOT.JIGGLE_DOWN_SPEED),
+          new WaitUntilIntakePivotStall()
         )
       )
     );
--- robotbase/src/main/java/frc/robot/commands/intakepivot/IntakePivotJiggle.java
+++ robotbase/src/main/java/frc/robot/commands/intakepivot/IntakePivotJiggle.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.intakePivot;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -8,24 +9,19 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.Robot;
 
-public class IntakePivotJiggle extends SequentialCommandGroup {
+public class IntakePivotJiggle extends RepeatCommand {
 
   public IntakePivotJiggle() {
     super(
-      new InstantCommand(() -> Robot.intakePivot.setDeployed(false)),
-      new RepeatCommand(
-        new SequentialCommandGroup(
-          new ParallelRaceGroup(
-            Robot.intakePivot.setSpeed(Constants.INTAKE_PIVOT.JIGGLE_UP_SPEED),
-            new WaitUntilIntakePivotStall(),
-            new WaitCommand(Constants.INTAKE_PIVOT.JIGGLE_UP_TIME)
-          ),
-          new ParallelRaceGroup(
-            Robot.intakePivot.setSpeed(
-              Constants.INTAKE_PIVOT.JIGGLE_DOWN_SPEED
-            ),
-            new WaitUntilIntakePivotStall()
-          )
+      new SequentialCommandGroup(
+        new ParallelRaceGroup(
+          Robot.intakePivot.setSpeed(Constants.INTAKE_PIVOT.JIGGLE_UP_SPEED),
+          new WaitUntilIntakePivotStall(),
+          new WaitCommand(Constants.INTAKE_PIVOT.JIGGLE_UP_TIME)
+        ),
+        new ParallelRaceGroup(
+          Robot.intakePivot.setSpeed(Constants.INTAKE_PIVOT.JIGGLE_DOWN_SPEED),
+          new WaitUntilIntakePivotStall()
         )
       )
     );
