da12061805560b458240f4d7b8b3bc689d082b36
Nathan2351 <nathan.larsen892@raypec.org>
2026-03-15 14:06:22 -0500

    finished up the testSwerve

--- robotbase/src/main/java/frc/robot/commands/drive/TestSwerve.java
+++ robotbase/src/main/java/frc/robot/commands/drive/TestSwerve.java
@@ -1,6 +1,6 @@
 package frc.robot.commands.drive;
 
-import static edu.wpi.first.units.Units.Value;
+import static edu.wpi.first.units.Units.Percent;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
@@ -12,9 +12,18 @@ public class TestSwerve extends SequentialCommandGroup {
   public TestSwerve() {
     super(
       new InstantCommand(() -> new ResetPerspective()),
-      new ParallelDeadlineGroup(new WaitCommand(1), new VelocityDrive()),
-      new ParallelDeadlineGroup(new WaitCommand(1), new VelocityDrive()),
-      new ParallelDeadlineGroup(new WaitCommand(1), new VelocityDrive())
+      new ParallelDeadlineGroup(
+        new WaitCommand(1),
+        new VelocityDrive(Percent.of(1), Percent.of(0), Percent.of(0))
+      ),
+      new ParallelDeadlineGroup(
+        new WaitCommand(1),
+        new VelocityDrive(Percent.of(1), Percent.of(0), Percent.of(0))
+      ),
+      new ParallelDeadlineGroup(
+        new WaitCommand(1),
+        new VelocityDrive(Percent.of(1), Percent.of(0), Percent.of(0))
+      )
     );
   }
 }
