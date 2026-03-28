fb8adb340adf0e6c7f349c47081b1eda8781c339
Tyson453 <tyson112305@gmail.com>
2026-03-28 18:43:27 -0500

    LeftTrenchBump

--- robotbase/src/main/java/frc/robot/commands/auto/RightTrenchBump.java
+++ robotbase/src/main/java/frc/robot/commands/auto/RightTrenchBump.java
@@ -6,7 +6,6 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.commands.auto.AutoMaker.Auto;
 import frc.robot.commands.drive.AutoTargetLock;
-import frc.robot.commands.intakepivot.AutoIntakePivotDeploy;
 import frc.robot.commands.intakepivot.IntakePivotDeploy;
 import frc.robot.commands.intakerunner.IntakeRunnerUntil;
 import frc.robot.commands.scoring.auto.AutoShoot;
@@ -30,7 +29,6 @@ public class RightTrenchBump extends AutoBase {
   public AutoRoutine getRoutine() {
     Auto auto = AutoMaker.newAuto(m_name);
     AutoTrajectory traj = auto.startTrajectory();
-    //traj.active().onTrue(new AutoIntakePivotDeploy());
 
     // First pass into neutral zone
     traj
