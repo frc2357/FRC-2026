6564aded4d58011a98390a101ae4b257f1537598
Nolan Campbell <nolanc1223@gmail.com>
2026-03-29 16:28:57 -0500

    Fix Swerve Offsets

--- robotbase/src/main/java/frc/robot/generated/TunerConstants.java
+++ robotbase/src/main/java/frc/robot/generated/TunerConstants.java
@@ -157,7 +157,7 @@ public class TunerConstants {
   private static final int kFrontLeftSteerMotorId = 12;
   private static final int kFrontLeftEncoderId = 19;
   private static final Angle kFrontLeftEncoderOffset = Rotations.of(
-    -0.100830078125
+    -0.104248046875
   );
   private static final boolean kFrontLeftSteerMotorInverted = true;
   private static final boolean kFrontLeftEncoderInverted = false;
@@ -170,7 +170,7 @@ public class TunerConstants {
   private static final int kFrontRightSteerMotorId = 14;
   private static final int kFrontRightEncoderId = 20;
   private static final Angle kFrontRightEncoderOffset = Rotations.of(
-    -0.00341796875
+    0.00341796875
   );
   private static final boolean kFrontRightSteerMotorInverted = true;
   private static final boolean kFrontRightEncoderInverted = false;
@@ -183,7 +183,7 @@ public class TunerConstants {
   private static final int kBackLeftSteerMotorId = 16;
   private static final int kBackLeftEncoderId = 21;
   private static final Angle kBackLeftEncoderOffset = Rotations.of(
-    -0.248779296875
+    0.297119140625
   );
   private static final boolean kBackLeftSteerMotorInverted = true;
   private static final boolean kBackLeftEncoderInverted = false;
@@ -196,7 +196,7 @@ public class TunerConstants {
   private static final int kBackRightSteerMotorId = 18;
   private static final int kBackRightEncoderId = 22;
   private static final Angle kBackRightEncoderOffset = Rotations.of(
-    -0.462890625
+    -0.457763671875
   );
   private static final boolean kBackRightSteerMotorInverted = true;
   private static final boolean kBackRightEncoderInverted = false;
