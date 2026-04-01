59b2a7997df6d4fd1a7ddc1508918458af386505
Nolan Campbell <nolanc1223@gmail.com>
2026-04-01 17:56:27 -0500

    Remove excess smart dashboard items

--- robotbase/src/main/java/frc/robot/commands/drive/SetIntakeDriveModifiers.java
+++ robotbase/src/main/java/frc/robot/commands/drive/SetIntakeDriveModifiers.java
@@ -1,43 +1,17 @@
 package frc.robot.commands.drive;
 
-import static edu.wpi.first.units.Units.Value;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.SWERVE;
 import frc.robot.Robot;
 
 public class SetIntakeDriveModifiers extends Command {
 
-  public SetIntakeDriveModifiers() {
-    SmartDashboard.putNumber(
-      "intake translation",
-      SWERVE.INTAKE_TRANSLATION_MODIFIER.in(Value)
-    );
-    SmartDashboard.putNumber(
-      "intake rotation",
-      SWERVE.INTAKE_ROTATION_MODIFIER.in(Value)
-    );
-  }
+  public SetIntakeDriveModifiers() {}
 
   @Override
   public void initialize() {
-    Robot.swerve.setTranslationModifier(
-      Value.of(
-        SmartDashboard.getNumber(
-          "intake translation",
-          SWERVE.INTAKE_TRANSLATION_MODIFIER.in(Value)
-        )
-      )
-    );
-    Robot.swerve.setRotationModifier(
-      Value.of(
-        SmartDashboard.getNumber(
-          "intake rotation",
-          SWERVE.INTAKE_ROTATION_MODIFIER.in(Value)
-        )
-      )
-    );
+    Robot.swerve.setTranslationModifier(SWERVE.INTAKE_TRANSLATION_MODIFIER);
+    Robot.swerve.setRotationModifier(SWERVE.INTAKE_ROTATION_MODIFIER);
   }
 
   @Override
