8214732f02af27cb2d038efcbd2776244b996a94
Tyson453 <tyson112305@gmail.com>
2026-03-31 21:11:23 -0500

    hood offset and intake stuff

--- robotbase/src/main/java/frc/robot/commands/intaking/TeleopIntake.java
+++ robotbase/src/main/java/frc/robot/commands/intaking/TeleopIntake.java
@@ -1,7 +1,10 @@
 package frc.robot.commands.intaking;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.INTAKE_PIVOT;
 import frc.robot.Constants.INTAKE_RUNNER;
+import frc.robot.Robot;
 import frc.robot.commands.intakepivot.IntakePivotDeploy;
 import frc.robot.commands.intakerunner.IntakeRunnerSetSpeed;
 
@@ -9,7 +12,10 @@ public class TeleopIntake extends ParallelCommandGroup {
 
   public TeleopIntake() {
     super(
-      new IntakePivotDeploy(),
+      new SequentialCommandGroup(
+        new IntakePivotDeploy(),
+        Robot.intakePivot.setSpeed(INTAKE_PIVOT.HOLD_DOWN_SPEED)
+      ),
       new IntakeRunnerSetSpeed(INTAKE_RUNNER.TELEOP_INTAKING_SPEED)
     );
   }
