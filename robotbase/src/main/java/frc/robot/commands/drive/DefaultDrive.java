083d41492c6619889d3ed93290cae16062cc3d96
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2026-04-09 18:20:42 -0500

    Implemented a Slew Rate limiter on the Default Drive command

--- robotbase/src/main/java/frc/robot/commands/drive/DefaultDrive.java
+++ robotbase/src/main/java/frc/robot/commands/drive/DefaultDrive.java
@@ -1,7 +1,10 @@
 package frc.robot.commands.drive;
 
 import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.Rotation;
+import static edu.wpi.first.units.Units.Value;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
@@ -15,6 +18,10 @@ public class DefaultDrive extends Command {
   Supplier<Dimensionless> m_y;
   Supplier<Dimensionless> m_rotation;
 
+  SlewRateLimiter m_LimiterX;
+  SlewRateLimiter m_LimiterY;
+  SlewRateLimiter m_LimiterRotation;
+
   public DefaultDrive(
     Supplier<Dimensionless> x,
     Supplier<Dimensionless> y,
@@ -24,30 +31,33 @@ public class DefaultDrive extends Command {
     m_x = x;
     m_y = y;
     m_rotation = rotation;
+
+    m_LimiterX = new SlewRateLimiter(.5);
+    m_LimiterY = new SlewRateLimiter(.5);
+    m_LimiterRotation = new SlewRateLimiter(.5);
   }
 
   @Override
   public void execute() {
-    if (
-      m_x.get().in(Percent) == 0 &&
-      m_y.get().in(Percent) == 0 &&
-      m_rotation.get().in(Percent) == 0
-    ) {
+    double m_xPercent = m_LimiterX.calculate(m_x.get().in(Value));
+    double m_yPercent = m_LimiterY.calculate(m_y.get().in(Value));
+    double m_rotationPercent = m_LimiterRotation.calculate(
+      m_rotation.get().in(Value)
+    );
+
+    if (m_xPercent == 0 && m_yPercent == 0 && m_rotationPercent == 0) {
       Robot.swerve.stopMotors();
     } else {
       Robot.swerve.driveFieldRelative(
-        m_y
-          .get()
-          .times(Constants.SWERVE.AXIS_MAX_SPEED)
-          .times(SWERVE.MAX_SPEED),
-        m_x
-          .get()
-          .times(Constants.SWERVE.AXIS_MAX_SPEED)
-          .times(SWERVE.MAX_SPEED),
-        m_rotation
-          .get()
-          .times(Constants.SWERVE.AXIS_MAX_ANGULAR_RATE)
-          .times(SWERVE.MAX_ANGULAR_RATE)
+        SWERVE.MAX_SPEED.times(Constants.SWERVE.AXIS_MAX_SPEED).times(
+          m_xPercent
+        ),
+        SWERVE.MAX_SPEED.times(Constants.SWERVE.AXIS_MAX_SPEED).times(
+          m_yPercent
+        ),
+        SWERVE.MAX_ANGULAR_RATE.times(
+          Constants.SWERVE.AXIS_MAX_ANGULAR_RATE
+        ).times(m_rotationPercent)
       );
     }
   }
