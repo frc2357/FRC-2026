eab315add583dcd4960e0da7f3890c64172e2d82
Nolan Campbell <nolanc1223@gmail.com>
2026-07-15 18:20:23 -0500

    Revert "Use absolute encoder on intake pivot (#229)"
    This reverts commit d582a5596d6c3a164f7f8c420ea8f3ea49bd1321.

--- robotbase/src/main/java/frc/robot/subsystems/IntakePivot.java
+++ robotbase/src/main/java/frc/robot/subsystems/IntakePivot.java
@@ -1,17 +1,24 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Value;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
@@ -93,6 +100,14 @@ public class IntakePivot extends SubsystemBase {
     return m_sparkSmartMotorController.getStatorCurrent();
   }
 
+  public AngularVelocity getVelocity() {
+    return RPM.of(m_motor.getEncoder().getVelocity());
+  }
+
+  public Dimensionless getAppliedOutput() {
+    return Value.of(m_motor.getAppliedOutput());
+  }
+
   public Command axisSpeed(Supplier<Dimensionless> axis) {
     return m_arm
       .set(() -> axis.get().times(INTAKE_PIVOT.AXIS_MAX_SPEED).in(Value))
@@ -101,6 +116,10 @@ public class IntakePivot extends SubsystemBase {
 
   public Command stopCommand() {
     return m_arm.set(0);
+  }
+
+  public Command zeroMotorEncoder() {
+    return new InstantCommand(() -> m_motor.getEncoder().setPosition(0));
   }
 
   public void stopMotor() {
@@ -114,12 +133,27 @@ public class IntakePivot extends SubsystemBase {
     ).debounce(INTAKE_PIVOT.TIME_TO_STALL.in(Seconds));
   }
 
+  // A trigger to check if the pivot is stalling based on motor velocity and applied output
+  public Trigger isIntakeVelocityStallingTrigger() {
+    return new Trigger(
+      () ->
+        getAppliedOutput().abs(Value) >=
+          INTAKE_PIVOT.VELOCITY_STALL_MIN_APPLIED_OUTPUT.in(Value) &&
+        getVelocity().abs(RotationsPerSecond) <=
+        INTAKE_PIVOT.VELOCITY_STALL_THRESHOLD.in(RotationsPerSecond)
+    ).debounce(INTAKE_PIVOT.TIME_TO_STALL.in(Seconds));
+  }
+
   public Trigger isAbovePositionTrigger(Angle targetAngle) {
-    return new Trigger(() -> true);
+    return new Trigger(() ->
+      Rotations.of(m_motor.getEncoder().getPosition()).gte(targetAngle)
+    );
   }
 
   public Trigger isBelowPositionTrigger(Angle targetAngle) {
-    return new Trigger(() -> true);
+    return new Trigger(() ->
+      Rotations.of(m_motor.getEncoder().getPosition()).lte(targetAngle)
+    );
   }
 
   public Trigger isInDeployedPositionTrigger() {
